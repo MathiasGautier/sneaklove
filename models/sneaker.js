78e7bbd1dc7a4dbc6e4e501d1a583dce914d914f
HAKIM BEN FADHEL <capzebib2@yahoo.fr>
2020-04-24 18:30:37 +0200

    error

--- models/sneaker.js
+++ models/sneaker.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-<<<<<<< HEAD
 const sneakerSchema = new Schema({
   name: String,
   ref: String,
@@ -12,18 +11,6 @@ const sneakerSchema = new Schema({
   image: String,
   price: String,
   id_tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }]
-=======
-const sneakerSchema = new Schema ({
-    name : String,
-    ref :String,
-    size: Number,
-    price:Number,
-    category :{type:String,
-    enum: ["men", "women", "kids"]},
-    image: String,
-    price : Number,
-    id_tags : [{type : Schema.Types.ObjectId, ref: "Tag"}]
->>>>>>> bf628b60ec5ddaee8805f4814a14d7ba710bbbfc
 });
 
 const Sneaker = mongoose.model("Shoes", sneakerSchema);
